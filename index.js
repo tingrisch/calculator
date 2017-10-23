2dbe4f75475e41bf31c84d2676020bbe9595f299
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-10-23 17:17:24 +0200

    gitignore added

---
+++ index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css';
+
+// 1. What state is there?
+// 2. When does it change?
+
+class Calculator extends React.Component {
+  state = {
+    displayValue: '0'
+  }
+
+  inputDigit = digit => {
+    this.setState({
+      displayValue: (this.state.displayValue != '0') ? this.state.displayValue + String(digit) : String(digit)
+    })
+  }
+
+  render() {
+    const { displayValue } = this.state;
+
+    return (
+      <div className="calculator">
+        <div className="calculator-display">{displayValue}</div>
+        <div className="calculator-keypad">
+          <div className="input-keys">
+            <div className="function-keys">
+              <button className="calculator-key key-clear">AC</button>
+              <button className="calculator-key key-sign">±</button>
+              <button className="calculator-key key-percent">%</button>
+            </div>
+            <div className="digit-keys">
+              <button className="calculator-key key-0" onClick={() => this.inputDigit(0)}>0</button>
+              <button className="calculator-key key-dot">●</button>
+              <button className="calculator-key key-1" onClick={() => this.inputDigit(1)}>1</button>
+              <button className="calculator-key key-2" onClick={() => this.inputDigit(2)}>2</button>
+              <button className="calculator-key key-3" onClick={() => this.inputDigit(3)}>3</button>
+              <button className="calculator-key key-4" onClick={() => this.inputDigit(4)}>4</button>
+              <button className="calculator-key key-5" onClick={() => this.inputDigit(5)}>5</button>
+              <button className="calculator-key key-6" onClick={() => this.inputDigit(6)}>6</button>
+              <button className="calculator-key key-7" onClick={() => this.inputDigit(7)}>7</button>
+              <button className="calculator-key key-8" onClick={() => this.inputDigit(8)}>8</button>
+              <button className="calculator-key key-9" onClick={() => this.inputDigit(9)}>9</button>
+            </div>
+          </div>
+          <div className="operator-keys">
+            <button className="calculator-key key-divide">÷</button>
+            <button className="calculator-key key-multiply">×</button>
+            <button className="calculator-key key-subtract">−</button>
+            <button className="calculator-key key-add">+</button>
+            <button className="calculator-key key-equals">=</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <div id="wrapper">
+    <Calculator/>
+  </div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = options => {
+  return {
+    entry: './index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [ 
+            'style-loader',
+            'css-loader'
+          ]
+        }
+      ],
+    },
+  }
+}
